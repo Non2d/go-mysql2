d1487f34c2f330d94629147bef868775f931b1c4
non2d <onedanijo11@gmail.com>
2024-01-08 02:58:54 +0900

    APIのパス環境変数，他のテーブル名でも対応できるように変更

--- golang/app/main.go
+++ golang/app/main.go
@@ -20,15 +20,15 @@ func main() {
 	// 環境変数からAPIのプレフィックスを取得
 	apiPrefix := os.Getenv("API_PREFIX")
 	if apiPrefix == "" {
-		apiPrefix = "/users" // デフォルトのプレフィックス
+		apiPrefix = "/" // デフォルトのプレフィックス
 	}
 
 	// ルートを設定（第一引数にエンドポイント、第二引数にハンドラーを指定）
-	e.GET(apiPrefix, handler.GetAllUser)
-	e.POST(apiPrefix, handler.PostNewUser)
-	e.GET(apiPrefix+"/:id", handler.GetOneUser)
-	e.PUT(apiPrefix+"/:id", handler.PutUser)
-	e.DELETE(apiPrefix+"/:id", handler.DeleteUser)
+	e.GET(apiPrefix+"users", handler.GetAllUser)
+	e.POST(apiPrefix+"users", handler.PostNewUser)
+	e.GET(apiPrefix+"users/:id", handler.GetOneUser)
+	e.PUT(apiPrefix+"users/:id", handler.PutUser)
+	e.DELETE(apiPrefix+"users/:id", handler.DeleteUser)
 
 	// サーバーをポート番号8080で起動
 	e.Logger.Fatal(e.Start(":8080"))
